d1dea72c52ca140f2aac3d3ed4dfcda3c1a53a5a
yd-03 <3nu4ydye57@gmail.com>
2024-07-07 12:26:23 +0900

    Update: INSERTを効率化

--- sql/example.sql
+++ sql/example.sql
@@ -11,5 +11,6 @@ CREATE TABLE happiness (
     gov_trust DECIMAL(16, 16) NOT NULL
 );
 
-INSERT INTO happiness (survey_yr, country, overall_rank, score, gdp, generosity, freedom, social_support, life_exp, gov_trust) VALUES (19, 'Finland', 1, 7.769, 1.340, 0.153, 0.596, 1.573, 0.986, 0.393);
-INSERT INTO happiness (survey_yr, country, overall_rank, score, gdp, generosity, freedom, social_support, life_exp, gov_trust) VALUES (19, 'Denmark', 2, 7.600, 1.383, 0.252, 0.592, 1.582, 0.996, 0.410);
+INSERT INTO happiness (survey_yr, country, overall_rank, score, gdp, generosity, freedom, social_support, life_exp, gov_trust) VALUES
+(19, 'Finland', 1, 7.769, 1.340, 0.153, 0.596, 1.573, 0.986, 0.393),
+(19, 'Denmark', 2, 7.600, 1.383, 0.252, 0.592, 1.582, 0.996, 0.410);
