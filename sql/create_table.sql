e83775e22d3ba6727cdbd0bce414c000a2cae122
yd-03 <3nu4ydye57@gmail.com>
2024-07-08 21:58:28 +0900

    Update: happinessテーブルのDECIMALの精度とスケールを更新する

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -2,11 +2,11 @@ CREATE TABLE IF NOT EXISTS happiness (
     survey_yr INTEGER NOT NULL,
     country VARCHAR(24) NOT NULL,
     overall_rank INTEGER NOT NULL,
-    score DECIMAL(10, 4) NOT NULL,
-    gdp DECIMAL(10, 5) NOT NULL,
-    generosity DECIMAL(10, 5) NOT NULL,
-    freedom DECIMAL(10, 5) NOT NULL,
-    social_support DECIMAL(10, 5) NOT NULL,
-    life_exp DECIMAL(10, 5) NOT NULL,
-    gov_trust DECIMAL(10, 5) NOT NULL
+    score DECIMAL(15, 14) NOT NULL,
+    gdp DECIMAL(17, 16) NOT NULL,
+    generosity DECIMAL(16, 16) NOT NULL,
+    freedom DECIMAL(16, 16) NOT NULL,
+    social_support DECIMAL(16, 15) NOT NULL,
+    life_exp DECIMAL(17, 16) NOT NULL,
+    gov_trust DECIMAL(16, 16) NOT NULL
 );
