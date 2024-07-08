91fe004d3daffd475ec590078f07152712cbab6a
yd-03 <3nu4ydye57@gmail.com>
2024-07-08 21:58:59 +0900

    Update: データベース接続とトランザクション処理を改善し、INSERTを効率化する

--- push.py
+++ push.py
@@ -2,8 +2,8 @@ from module.db_utils import (
     get_db_connection,
     load_environment_variables,
     load_sql,
-    insert_data_from_single_csv,
 )
+import pandas as pd
 
 
 # 環境変数をロード
@@ -13,12 +13,9 @@ load_environment_variables()
 create_table_sql = load_sql("./sql/create_table.sql")
 insert_sql = load_sql("./sql/insert_data.sql")
 
-# CSVファイルが格納されているディレクトリ
-csv_directory = "./raw_data"
-
-# 処理する単一のCSVファイルとその年
-csv_file_path = "./raw_data/2015.csv"
-survey_yr = 2015
+# CSVファイルを読み込む
+csv_file_path = "./results.csv"
+df = pd.read_csv(csv_file_path)
 
 try:
     # データベース接続を取得し、自動でクローズする
@@ -27,14 +24,23 @@ try:
         with conn.cursor() as cursor:
             # テーブルを作成する
             cursor.execute(create_table_sql)
-            # ディレクトリ内のすべてのCSVファイルを処理
-            # for filename in os.listdir(csv_directory):
-            #     if filename.endswith(".csv"):
-            #         csv_file_path = os.path.join(csv_directory, filename)
-            #         survey_yr = int(filename.split(".")[0])  # ファイル名から年を取得
-            #         insert_data_from_csv(cursor, insert_sql, csv_file_path, survey_yr)
-            # 単一のCSVファイルを処理
-            insert_data_from_single_csv(cursor, insert_sql, csv_file_path, survey_yr)
+            # データを挿入する
+            for _, row in df.iterrows():
+                cursor.execute(
+                    insert_sql,
+                    (
+                        row["survey_yr"],
+                        row["country"],
+                        row["overall_rank"],
+                        row["score"],
+                        row["gdp"],
+                        row["generosity"],
+                        row["freedom"],
+                        row["social_support"],
+                        row["life_exp"],
+                        row["gov_trust"],
+                    ),
+                )
             # コミット
             conn.commit()
 except Exception as e:
